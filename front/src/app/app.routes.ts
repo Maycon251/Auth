35adae19e5818b66f894c6507ad94157f4debae7
Lucas Martins <lucasifbaeuna@gmail.com>
2025-03-30 23:26:08 -0300

    feat: create service login

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -4,11 +4,20 @@ export const routes: Routes = [
   {
     path: 'register',
     loadComponent: () =>
-      import('./pages/register/register.component').then(c => c.RegisterComponent),
+      import('./pages/register/register.component').then(
+        (c) => c.RegisterComponent
+      ),
   },
   {
     path: '',
     loadComponent: () =>
-      import('./pages/login/login.component').then(c => c.LoginComponent),
+      import('./pages/login/login.component').then((c) => c.LoginComponent),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./pages/not-found/not-found.component').then(
+        (c) => c.NotFoundComponent
+      ),
   },
 ];
--- front/src/app/pages/login/login.component.ts
+++ front/src/app/pages/login/login.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { LoginService } from './service/login.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  errorMessage: string = '';
+  registerForm!: FormGroup;
 
+  private _loginService = inject(LoginService);
+  private _formBuilder = inject(FormBuilder);
+  private _router = inject(Router);
+
+  ngOnInit(): void {
+    this.registerForm = this._formBuilder.group({
+      nickname: [''],
+      password: [''],
+    });
+  }
+
+  login() {
+    const user = this.registerForm.value;
+    this._loginService.loginUser(user).subscribe(
+      (response) => {
+        localStorage.setItem('token', response.token);
+        //TODO cria page Dashboard
+        // this._router.navigate(['/dashboard']);
+      },
+      (error) => {
+        if (error.status === 401) {
+          this.errorMessage = 'Usuário ou senha inválidos!';
+        } else if (error.status === 500) {
+          this.errorMessage = 'Erro no servidor!';
+        } else if (error.status === 403) {
+          //TODO criar page de erro 403
+          // this._router.navigate(['/erro-api']);
+        }
+      }
+    );
+  }
 }
