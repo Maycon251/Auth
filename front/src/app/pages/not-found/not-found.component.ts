966f981ef5364d70bfc54878ab1e3bdc99ccc295
Lucas Martins <lucasifbaeuna@gmail.com>
2025-04-05 11:54:53 -0300

    feat: Create page not found

--- front/src/app/pages/not-found/not-found.component.ts
+++ front/src/app/pages/not-found/not-found.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { StringConstants } from './../../shared/constants/string-constans';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
   imports: [],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.scss'
+  styleUrl: './not-found.component.scss',
 })
 export class NotFoundComponent {
+  // Constants
+  title = StringConstants.PAGE_NOT_FOUND_TITLE;
+  text = StringConstants.PAGE_NOT_FOUND_TEXT;
+  button = StringConstants.PAGE_NOT_FOUND_BUTTON;
 
+  private _router = inject(Router);
+
+  gotoHome() {
+    this._router.navigate(['/']);
+  }
 }
